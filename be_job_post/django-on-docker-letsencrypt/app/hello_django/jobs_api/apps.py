8740e76e94bc0734cacd5337b8ec3e1d53377010
MikeKravchenko <kravchenko.soft@gmail.com>
2020-08-19 03:06:01 -0400

    better structure

--- be_job_post/django-on-docker-letsencrypt/app/hello_django/jobs_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobsApiConfig(AppConfig):
-    name = 'jobs_api'
--- be_job_post/django-on-docker-letsencrypt/app/hello_django/jobs_api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import JobPost
-
-
-class JobPostSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = JobPost
-        fields = ['id', 'title', 'description', 'categories', 'applicants', 'contact']
